99f3b0c76a38a9e7107290c6f2beece321b39af8
AnnaLuiza-M <lulumoreira150@gmail.com>
2025-07-09 19:44:03 -0300

    find implementations

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.util.Date;
+import java.util.List;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -11,14 +12,29 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		
-		
-		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		Seller seller4 = sellerDao.findById(7);
 		
 		System.out.println(seller4);
+		
+		System.out.println("Find by dep test");
+		
+		Department department = new Department(2, null);
+		List<Seller> list = sellerDao.findByDepartment(department);
+		
+		for(Seller obj : list) {
+			System.out.println(obj);
+		}
+		
+
+		System.out.println("Find all test");
+		
+		List<Seller> list2 = sellerDao.findAll();
+		
+		for(Seller obj : list2) {
+			System.out.println(obj);
+		}
 	}
 
 }
